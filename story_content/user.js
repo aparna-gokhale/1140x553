762a316d58c97026c8782f34f0e26054156a354c
aparna-gokhale <aparnagokhale131096@gmail.com>
2025-04-13 17:12:57 +0530

    yes

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,31 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "5vK84iQCOUa":
+        Script1();
+        break;
   }
+}
+
+function Script1()
+{
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||
+(!document.mozFullScreen && !document.webkitIsFullScreen)) {
+if (document.documentElement.requestFullScreen) {
+document.documentElement.requestFullScreen();
+} else if (document.documentElement.mozRequestFullScreen) {
+document.documentElement.mozRequestFullScreen();
+} else if (document.documentElement.webkitRequestFullScreen) {
+document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+}
+} else {
+if (document.cancelFullScreen) {
+document.cancelFullScreen();
+} else if (document.mozCancelFullScreen) {
+document.mozCancelFullScreen();
+} else if (document.webkitCancelFullScreen) {
+document.webkitCancelFullScreen();
+}
+}
 }
 
